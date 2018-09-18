b51e179ba41e239f713fea8dace96eccc1b3e788
xiaoping.xu <xiaoping.xu@weimob.com>
2018-09-18 18:45:53 +0800

    react全家桶

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import getRouter from './router/router'
+
+
+if (module.hot) {
+    module.hot.accept();
+}
+
+ReactDom.render(
+    getRouter(), document.getElementById('app'));
---
+++ src/pages/Page1/Page1.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Page1 extends Component {
+    render() {
+        return (
+            <div>
+                this is Page1~_______________刷新测试！111附近看到附近地块框架
+                <div>fdfdsklfjdk</div>
+            </div>
+        )
+    }
+}
