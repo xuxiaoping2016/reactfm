069ff381642f00b6ab99b1e32bf59bd86aa22be3
xiaoping.xu <xiaoping.xu@weimob.com>
2018-11-20 14:07:21 +0800

    分割代码前

--- src/router/router.js
+++ src/router/router.js
@@ -3,29 +3,41 @@ import Bundle from './Bundle';
 import {Route, Switch} from 'react-router-dom';
 
 import Loading from 'components/Loading/Loading.jsx'
-import Home from 'bundle-loader?lazy&name=home!pages/Home/Home';
-import Page1 from 'bundle-loader?lazy&name=page1!pages/Page1/Page1';
-import Counter from 'bundle-loader?lazy&name=counter!pages/Counter/Counter'
-import UserInfo from 'bundle-loader?lazy&name=userinfo!pages/UserInfo/UserInfo';
-import NotFound from 'bundle-loader?lazy&name=notFound!pages/NotFound/NotFound.jsx';
+import Home from 'pages/Home/Home';
+import Page1 from 'pages/Page1/Page1';
+import Counter from 'pages/Counter/Counter'
+import UserInfo from 'pages/UserInfo/UserInfo';
+import NotFound from 'pages/NotFound/NotFound.jsx';
+
+// import Loading from 'components/Loading/Loading.jsx'
+// import Home from 'bundle-loader?lazy&name=home!pages/Home/Home';
+// import Page1 from 'bundle-loader?lazy&name=page1!pages/Page1/Page1';
+// import Counter from 'bundle-loader?lazy&name=counter!pages/Counter/Counter'
+// import UserInfo from 'bundle-loader?lazy&name=userinfo!pages/UserInfo/UserInfo';
+// import NotFound from 'bundle-loader?lazy&name=notFound!pages/NotFound/NotFound.jsx';
 
 
-const createComponent = (Com) => (props) => (
-    <Bundle load={Com}>
-        {
-            (Com) => Com ? <Com {...props} /> : <Loading/>
-        }
-    </Bundle>
-);
+// const createComponent = (Com) => (props) => (
+//     <Bundle load={Com}>
+//         {
+//             (Com) => Com ? <Com {...props} /> : <Loading/>
+//         }
+//     </Bundle>
+// );
 
 
 const getRouter = () => (
     <Switch>
-        <Route exact path="/" component={createComponent(Home)}/>
+        <Route exact path="/" component={Home}/>
+        <Route path="/page1" component={Page1}/>
+        <Route path="/counter" component={Counter}/>
+        <Route path="/userinfo" component={UserInfo}/>
+        <Route component={NotFound}/>
+        {/* <Route exact path="/" component={createComponent(Home)}/>
         <Route path="/page1" component={createComponent(Page1)}/>
         <Route path="/counter" component={createComponent(Counter)}/>
         <Route path="/userinfo" component={createComponent(UserInfo)}/>
-        <Route component={createComponent(NotFound)}/>
+        <Route component={createComponent(NotFound)}/> */}
     </Switch>
 );
 
