0df9accc81be3b34e01270b175db849fc564ca3b
xiaoping.xu <xiaoping.xu@weimob.com>
2018-11-21 16:22:26 +0800

    tijia

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Bundle from './Bundle';
 import {Route,IndexRedirect,Router, browserHistory} from 'react-router';
 
 import Loading from 'components/Loading/Loading.jsx'
@@ -8,35 +7,34 @@ import Home from 'pages/Home/Home';
 import Page1 from 'pages/Page1/Page1';
 import Counter from 'pages/Counter/Counter'
 import UserInfo from 'pages/UserInfo/UserInfo';
-import NotFound from 'pages/NotFound/NotFound.jsx';
-
-
-// https://blog.csdn.net/qq_27626333/article/details/76228578
-// const Page1 = (location,cb) => 
-//     require.ensure([], (require) => cb(require('pages/Page1/Page1').default),'page1')
+import NotFound from 'pages/NotFound/NotFound.jsx'
+// const Page1 = (location,cb) =>
+//     require.ensure([], (require) => cb(null,require('pages/Page1/Page1').default),'page1')
 // const Counter = (location,cb) => 
-//     require.ensure([], (require) => cb(require('pages/Counter/Counter').default),'counter')
+//     require.ensure([], (require) => cb(null,require('pages/Counter/Counter').default),'counter')
 
 // const UserInfo =(location,cb) => 
-//     require.ensure([], (require) => cb(require('pages/UserInfo/UserInfo').default), 'userinfo')
+//     require.ensure([], (require) => cb(null,require('pages/UserInfo/UserInfo').default), 'userinfo')
 // const NotFound = (location,cb) => 
-//     require.ensure([], (require) => cb(require('pages/NotFound/NotFound.jsx').default), 'notfound')
+//     require.ensure([], (require) => cb(null,require('pages/NotFound/NotFound.jsx').default), 'notfound')
 
-// function getComponent(Com){
-//     console.log(Com.default)
-//     return Com.default
-// }
-
-
+export const getModule = (loader, exportName) => (nextState, cb) => loader(module => exportName
+    ? cb(null, module[exportName])
+    : cb(null, module))
+    
 const getRouter = () => (
     <Router history={browserHistory}>
         <Route path="/" component={App}>
             <IndexRedirect to="home" />
             <Route path="/home" component={Home}/>
-            <Route path="/page1" component={Page1}/>
-            <Route path="/counter" component={Counter}/>
-            <Route path="/userinfo" component={UserInfo}/>
-            {/* <Route path="page1" getComponent={Page1}/> */}
+            {/* <Route path="/page1" getComponent={Page1}/>
+            <Route path="/counter" getComponent={Counter}/>
+            <Route path="/userinfo" getComponent={UserInfo}/>
+            <Route path="/notfound" getComponent={NotFound}/> */}
+            <Route path="/page1" getComponent={getModule(Page1)}/>
+            <Route path="/counter" getComponent={getModule(Counter)}/>
+            <Route path="/userinfo" getComponent={getModule(UserInfo)}/>
+            <Route path="/notfound" getComponent={getModule(NotFound)}/>
         </Route>
     </Router>
 );
