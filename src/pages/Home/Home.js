ec9b22d664b199029b5f3fbce2cdcc0d0ac5272c
xiaoping.xu <xiaoping.xu@weimob.com>
2018-11-28 16:59:53 +0800

    tj

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,30 +1,41 @@
 import React, { Component } from 'react';
-// import './style.css'
-import './style.scss'
+import XLSX from 'xlsx'
 
-import im from '../../images/wait.png'
 export default class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+           
         }
     }
 
-    _handleClick() {
-        this.setState({
-            count: ++this.state.count
-        });
+    componentDidMount(){
+        const reader = new FileReader();
+        reader.onload= function(res){
+            console.log(res)
+        }
+        
+        reader.readAsDataURL()
+    }
+    
+    onReadImg(e) {
+        console.log(e.target.files[0]);
+        const reader = new FileReader();
+        // 读取文件内容，结果用data:url的字符串形式表示
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload = function(e) {
+            console.log(e.target.result);  // 上传的图片的编码
+            this.setState({
+                previewPic: e.target.result
+            });
+        }.bind(this);
     }
 
     render(){
         return (
             <div className="container">
-                <p className="scss">测试scss 样式文件!!!!!!!!!!</p>
-                <img src={im}/>
-                this is home~~fdfkd<br/>
-                当前计数：{this.state.count}<br/>
-                <button onClick={() => this._handleClick()}>自增</button>
+                <input type="file" ref="file"/>
+                <button onClick={this.onReadImg}>读取图片</button>
             </div>
         )
     }
