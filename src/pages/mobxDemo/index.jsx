c765a086f3013852b1593fe7c619c58f463c373b
xiaoping.xu <xiaoping.xu@weimob.com>
2018-12-25 14:28:44 +0800

    t

--- src/pages/mobxDemo/index.jsx
+++ src/pages/mobxDemo/index.jsx
@@ -17,6 +17,8 @@ export default class MobxDemo extends Component {
                     <MenuItem key="2"><Link to="/mobxdemo/child2">关于@observer的一些说明</Link></MenuItem>
                 </Menu>
                 <p>Mobx使用详解<br/>https://www.jianshu.com/p/505d9d9fe36a</p>
+                <p>MobX 核心源码解析<br/>https://segmentfault.com/p/1210000010496905</p>
+                <p>Mobx 思想的实现原理<br/>https://www.jianshu.com/p/8e9ab3f26162</p>
                 {this.props.children}
             </div>
         )
