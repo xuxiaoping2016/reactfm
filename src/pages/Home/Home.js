d39f6cfb974a42d07b2c7309f7fb457fb3ea0942
xiaoping.xu <xiaoping.xu@weimob.com>
2019-01-09 18:44:05 +0800

    r

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,46 +1,23 @@
 import React, { Component } from 'react';
-
-
-
-
-
-const validateFormat = function(format) {
-    if (format === undefined) {
-    throw new Error('invariant requires an error message argument');
+  class IncreasingCounter {
+    _count = 0
+    get value() {
+        console.log('Getting the current value!');
+        return this._count;
     }
-}
-  
-function invariant(condition, format, a, b, c, d, e, f) {
-    validateFormat(format);
-  
-    if (!condition) {
-      let error;
-      if (format === undefined) {
-        error = new Error(
-          'Minified exception occurred; use the non-minified dev environment ' +
-            'for the full error message and additional helpful warnings.',
-        );
-      } else {
-        const args = [a, b, c, d, e, f];
-        console.log(args)
-        let argIndex = 0;
-        error = new Error(
-          format.replace(/%s/g, function() {
-            return args[argIndex++];
-          }),
-        );
-        error.name = 'Invariant Violation';
-      }
-  
-      error.framesToPop = 1; // we don't care about invariant's own frame
-      console.log(error)
-      throw error;
+    increment() {
+        this._count++;
     }
   }
 
+
+
 export default class Home extends Component {
     componentDidMount(){
-        invariant(false,'hello %s word %s %s %s %s 我的 %s','1','2','3','4','5','6')
+        var i = new IncreasingCounter()
+        console.log(i.value)
+        i.increment()
+        console.log(i.value)
     }
 
     render(){
