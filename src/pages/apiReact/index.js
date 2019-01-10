fb1fba8f4368ccbc1e28ad326f07cbd8f549bdff
xiaoping.xu <xiaoping.xu@weimob.com>
2019-01-10 19:35:44 +0800

    t

--- src/pages/apiReact/index.js
+++ src/pages/apiReact/index.js
@@ -4,19 +4,66 @@ class ApiReact extends Component {
     constructor(props){
         super(props)
         this.state ={
-            name:'xuxia'
+            numberFunction:0,
+            numberObject: 0
         }
     }
+
+    changeNumberObject=()=>{
+
+        this.setState(
+            {numberObject:this.state.numberObject+1}
+        );
+    
+         this.setState(
+            {numberObject:this.state.numberObject+1}
+        );
+    
+         this.setState(
+            {numberObject:this.state.numberObject+1}
+         );
+    
+          this.setState(
+            {numberObject:this.state.numberObject+1}
+         );
+         //只有最后这个setState才生效 
+    };
+
+    changeNumberFunction=()=>{
+
+        this.setState((preState)=>{
+            return  {numberFunction:preState.numberFunction+1}
+        })
+    
+        this.setState((preState)=>{
+            return  {numberFunction:preState.numberFunction+1}
+        })
+    
+        this.setState((preState)=>{
+            return  {numberFunction:preState.numberFunction+1}
+        })
+    
+        this.setState((preState)=>{
+            return  {numberFunction:preState.numberFunction+1}
+        })
+        //每个都回执行 
+    };
+    
+    componentDidUpdate(){
+        console.log(`The expected  numberObject is 4,real value is ${this.state.numberObject}`);
+        console.log(`The expected  numberFunction is 4,real value is ${this.state.numberFunction}`);
+    }
+
     render() {
         
         return (
             <div>
-                {this.props.children}
+                <span onClick={this.changeNumberObject}  style={{marginRight:"20px"}}>changeNumberObject</span>
+                <span onClick={this.changeNumberFunction}>changeNumberFunction</span>
             </div>
         )
     }
 }
 
-console.log("ApiReact",ApiReact)
-console.log(<ApiReact>fdf</ApiReact>)
+
 export default ApiReact;
