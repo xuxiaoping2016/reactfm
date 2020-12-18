ebcb237a337450dc1dc3be69468a6fb1ca97146f
xiaoping.xu <xiaping.xu@weimob.com>
2020-12-18 15:15:58 +0800

    d

--- src/pages/clipboard/targetInput.tsx
+++ src/pages/clipboard/targetInput.tsx
@@ -6,12 +6,12 @@ const TargetIput = () => {
     useEffect(()=>{
         const cName = '.'+(ref.current as any).className;
         var clipboard = new ClipboardJS(cName);
-        const fn = function(e: any) {
-            console.log(e);
+        const fn = function(e: any,status:string) {
+            console.log(e,status);
         };
-        clipboard.on('success', fn);
+        clipboard.on('success', e => fn(e,'success'));
 
-        clipboard.on('error', fn);
+        clipboard.on('error', e => fn(e,'error'));
 
         return () => {
             clipboard.destroy();
@@ -21,10 +21,27 @@ const TargetIput = () => {
     const onChange = () => {
 
     }
+
+
+    const copyContent = (id:number) => {
+        const clipboard = new ClipboardJS(`#${id}`, {
+            text: function(trigger) {
+                return trigger.getAttribute('aria-label') || 'default text ';
+            }
+        });
+        clipboard.destroy();
+    }
     return (
         <div>
             <input id="foo" type="text" value="hello" onChange={onChange}/>
             <button className="btn" ref={ref} data-clipboard-action="copy" data-clipboard-target="#foo">Copy</button>
+
+            <button id="#1234" onClick={() => copyContent(1234)} data-clipboard-text="Just because you can doesn't mean you should — clipboard.js">
+                Copy to clipboard 1234
+            </button>
+            <button id="#5678" onClick={() => copyContent(5678)} data-clipboard-text="Just because you can doesn't mean you should — clipboard.js">
+                Copy to clipboard 5678
+            </button>
         </div>
     )
 }
